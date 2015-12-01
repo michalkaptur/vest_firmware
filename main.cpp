37f47fd0475b0a422f8dcd6fa9d01142175eeccd
edd44 <kaptur.michal@gmail.com>
2015-12-01 17:52:50 +0100

    simple sequence player

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@
 #define LED2 6
 #define LED3 9
 
+const char output_map[3] = { LED1, LED2, LED3 };
 
-#define MAX_SEQUENCE_LENGTH 3
-#define NR_OF_SEQUENCES 3
-const byte sequences[NR_OF_SEQUENCES][MAX_SEQUENCE_LENGTH+2] = {
-    {'a',3,LED1,LED2,LED3},
-    {'b',3,LED2,LED1,LED3},
-    {'c',2,LED3,LED2,0}
-};
-
-volatile static byte out_level(200);
-const byte out_config_step(20);
+volatile static short on_time(300);
+volatile static short off_time(30);
+volatile static byte out_level(130);
+const byte out_config_step(30);
+const int on_time_step(70);
 
 void config(byte _byte);
-void run_sequence(byte _byte);
+void add_to_sequence(char out_nr);
+void run_sequence();
 void turn_output_on(byte out_nr, int time);
+void flash(int time);
+
+char buffer[10];
+byte buffer_counter;
 
 void setup(){
     Serial.begin(9600);
@@ -26,6 +27,7 @@ void setup(){
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
+    buffer_counter = 0;
 }
 
 
@@ -35,8 +37,10 @@ void loop()
         if (Serial.available())
         {
             byte rx = Serial.read();
-            if (rx >= 'a' and rx <='c')
-                run_sequence(rx);
+            if (rx >= '0' and rx <='2')
+                add_to_sequence(rx);
+            else if (rx == '#')
+                run_sequence();
             else
                 config(rx);
         }
@@ -51,27 +55,31 @@ void config(byte _byte)
         break;
     case '-':
         out_level = (out_config_step < out_level) ? out_level-out_config_step : 0;
+    case '>':
+        on_time += on_time_step;
+        break;
+    case '<':
+        on_time = (on_time_step < on_time) ? on_time-on_time_step : 0;
+        break;
     default:
         break;
     }
-    analogWrite(LED1, out_level);
-    analogWrite(LED2, out_level);
-    analogWrite(LED3, out_level);
-    delay(500);
-    analogWrite(LED1, 0);
-    analogWrite(LED2, 0);
-    analogWrite(LED3, 0);
+    flash(on_time);
 }
 
-void run_sequence(byte _byte){
-    byte seq_nr(_byte-'a');
-    if (seq_nr < NR_OF_SEQUENCES) {
-        for(byte i=0; i<sequences[seq_nr][1];++i)
-        {
-            Serial.println(sequences[seq_nr][i+2]);
-            turn_output_on(sequences[seq_nr][i+2],200);
-        }
+void add_to_sequence(char out_nr)
+{
+    buffer[buffer_counter] = out_nr-'0';
+    buffer_counter++;
+}
+
+void run_sequence(){
+    for (byte i = 0 ; i < buffer_counter; ++i)
+    {
+        turn_output_on(output_map[buffer[i]], on_time);
+        delay(off_time);
     }
+    buffer_counter = 0;
 }
 
 void turn_output_on(byte out_nr, int time)
@@ -79,4 +87,14 @@ void turn_output_on(byte out_nr, int time)
     analogWrite(out_nr, out_level);
     delay(time);
     analogWrite(out_nr, 0);
+}
+
+void flash(int time){
+    analogWrite(LED1, out_level);
+    analogWrite(LED2, out_level);
+    analogWrite(LED3, out_level);
+    delay(time);
+    analogWrite(LED1, 0);
+    analogWrite(LED2, 0);
+    analogWrite(LED3, 0);
 }
