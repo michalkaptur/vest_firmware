e5433eb7010c47caf64a301aca82ae62b122a11a
edd44 <kaptur.michal@gmail.com>
2016-02-01 20:11:24 +0100

    simple packet formatter addon

--- tools/checksum.py
+++ tools/checksum.py
@@ -6,3 +6,5 @@ string=str(sys.argv[1])
 for i in string:
 	sum=sum+ord(i)
 print(sum%100)
+print('[D#'+string+'#'+str((sum+ord('D'))%100).zfill(2)+']')
+print('[C#'+string+'#'+str((sum+ord('C'))%100).zfill(2)+']')
